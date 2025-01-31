9afdb79c7cad4bfb78c514bf74748a8833f5f911
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2025-01-31 14:52:37 +0100

    Add Azure Resource Group and Host Pool configuration for AVD

--- main.tf
+++ main.tf
@@ -19,3 +19,26 @@ terraform {
 provider "azurerm" {
   features {}
 }
+# This resource block defines an Azure Resource Group named "rg-AVD-int-dewc-1"
+# in the location specified by the variable 'var.location'. The resource group
+# is tagged with the location "germanywestcentral" and the owner "Olad, Koosha".
+resource "azurerm_resource_group" "rg-avd" {
+  name     = var.resource_group_name
+  location = var.location
+  tags = {
+    Location = "germanywestcentral"
+    Owner    = "Olad, Koosha"
+  }
+}
+# AVD Host Pool
+resource "azurerm_virtual_desktop_host_pool" "avd_host_pool" {
+  name                = "hp-avd-int-dewc-1" # Example name hp-AVD-int-dewc-1
+  location            = azurerm_resource_group.rg-avd.location
+  resource_group_name = azurerm_resource_group.rg-avd.name
+
+  type               = var.host_pool_type     # "Pooled" or "Personal" (from variables.tf)
+  load_balancer_type = var.load_balancer_type # "DepthFirst" (fills one host at a time)
+  // validation_environment = true  # Set to "true" for production
+  friendly_name = "AVD Host Pool - Production"
+  description   = "Host pool for remote desktops"
+}
