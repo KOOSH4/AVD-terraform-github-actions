ba661bc0e3f080a9d1187aac384e150471f398bc
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2025-01-31 13:31:53 +0100

    Fix comment punctuation in Terraform configuration

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 
-  # Update this block with the location of your terraform state file
+  # Update this block with the location of your terraform state file.
   backend "azurerm" {
     resource_group_name  = "rg-AVD-int-dewc-2"
     storage_account_name = "stavdtfdewc2"
